0b807970fffaebc709b8262b64575fcb7f264720
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-01-11 00:04:18 +0100

    Move admin urls import

--- urls.py
+++ urls.py
@@ -7,14 +7,12 @@ from registration.views import activate
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^i4p/', include('i4p.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
     url(r'^$', direct_to_template, {'template' : 'base.html'}, name="i4p-index"),
     (r'^accounts/', include('apps.member.backend.urls')),
+    
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
 )
