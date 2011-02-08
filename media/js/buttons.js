668a8bda7a1ee7ebc9b93d4e38d0296a9bb3a768
Vincent Charrier <vincent.charrier@skwid-creation.fr>
2011-02-08 22:25:11 +0100

    Integration of project's creation page : body full finished, but not the opening grey colum

---
+++ media/js/buttons.js
@@ -0,0 +1,13 @@
+jQuery.ready(function() {
+  jQuery('a.minibutton').bind({
+    mousedown: function() {
+      jQuery(this).addClass('mousedown');
+    },
+    blur: function() {
+      jQuery(this).removeClass('mousedown');
+    },
+    mouseup: function() {
+      jQuery(this).removeClass('mousedown');
+    }
+  });
+});
