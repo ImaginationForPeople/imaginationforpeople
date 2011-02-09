a718491fb11ca2d3676cede22a8e6d1825ec795c
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-02-09 10:11:09 +0100

    WIP : creation view (#13)

--- apps/project_sheet/views.py
+++ apps/project_sheet/views.py
@@ -1,12 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from .forms import I4pProjectTitleFieldForm,\
+    I4pProjectBaselineFieldForm, I4pProjectAboutFieldForm,\
+    I4pProjectUniquenessFieldForm, I4pProjectValueFieldForm,\
+    I4pProjectScalabilityFieldForm
 from django.shortcuts import render_to_response
-from django.template import RequestContext
-from .models import GenericPage
+from django.template.context import RequestContext
+from django.utils.translation import ugettext_lazy as _
 
-def manifesto(request):
-
-    manifesto_copy = GenericPage.objects.get(title='manifeste')
-
-    # render the response
-    return render_to_response('default.html', 
-                              {'manifesto_copy':manifesto_copy}, 
-                              context_instance=RequestContext(request))
+def edit_project_sheet(request):
+    title_form = I4pProjectTitleFieldForm(initial={'title' : _("my project title")})
+    baseline_form = I4pProjectBaselineFieldForm(initial={'baseline' : _("my project\'s baseline")})
+    about_form = I4pProjectAboutFieldForm()
+    uniqueness_form = I4pProjectUniquenessFieldForm()
+    value_form = I4pProjectValueFieldForm()
+    scalability_form = I4pProjectScalabilityFieldForm()
+                                     
+    return render_to_response("project_sheet.html",
+                              {'title_form' : title_form,
+                               'baseline_form' : baseline_form,
+                               'about_form' : about_form,
+                               'uniqueness_form' : uniqueness_form,
+                               'value_form' : value_form,
+                               'scalability_form' : scalability_form},
+                              context_instance = RequestContext(request))
