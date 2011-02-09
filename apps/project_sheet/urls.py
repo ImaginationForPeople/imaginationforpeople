717481641598c67d9a25703870b4372c4a281785
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-02-09 10:08:28 +0100

    Update urls

--- apps/project_sheet/urls.py
+++ apps/project_sheet/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls.defaults import *
+from .views import edit_project_sheet
 
-# place app url patterns here
+urlpatterns = patterns('',
+    url(r'^create/$', edit_project_sheet, name='project_sheet_edit'),
+)
--- urls.py
+++ urls.py
@@ -5,12 +5,12 @@ from django.views.generic.simple import direct_to_template, redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from apps.project_sheet.views import manifesto
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
+
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
@@ -28,11 +28,11 @@ if "privatebeta" in settings.INSTALLED_APPS:
         url(r'^beta/$', 'privatebeta.views.invite', name='privatebeta_invite'),
         url(r'^beta/sent/$', 'privatebeta.views.sent', name='privatebeta_sent'),
         url(r'^beta/manifesto/$', direct_to_template, {'template': 'manifesto.html'}, name='manifesto'),
-        #url(r'^beta/manifesto/$', manifesto, name='manifesto'),
     )
 else:
     urlpatterns += patterns('',
         url(r'^$', direct_to_template, {'template': 'base.html'}, name='i4p-index'),
+        url(r'^project/', include('apps.project_sheet.urls')),
         url(r'^accounts/', include('userena.urls')),
     )
 
