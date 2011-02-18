9d14ee8013fe46061928f86d13266e2b25bcdc0d
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-02-18 02:00:28 +0100

    * display tags on right sidebar * ajax editing for value, scalability and uniqueness section

--- apps/project_sheet/ajax.py
+++ apps/project_sheet/ajax.py
@@ -1,11 +1,13 @@
+from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404
+from django.utils import simplejson
+
 from dajax.core import Dajax
 from dajaxice.core import dajaxice_functions
 
 from .models import I4pProject
 from .forms import I4pProjectThemesForm
-
-
+from .views import get_or_create_project
 
 def project_update_themes(request, themes_form, project_slug):
     """
@@ -28,6 +30,28 @@ def project_update_themes(request, themes_form, project_slug):
 
     return dajax.json()
 
+
+def project_sheet_edit_field(request, fieldname, value, project_slug=None):
+    """
+    Edit a text field
+    """
+    dajax = Dajax()
+
+    FieldForm = modelform_factory(I4pProject, fields=(fieldname,))
+
+    project = get_or_create_project(request, project_slug)
+
+    form = FieldForm({fieldname: value}, instance=project)
+    if form.is_valid():
+        form.save()
+
+    # Redirect to project sheet if it was created
+    if not project_slug:
+        dajax.redirect("%s#%s" % (project.get_absolute_url(), fieldname))
+
+    return dajax.json()
+
+dajaxice_functions.register(project_sheet_edit_field)
 dajaxice_functions.register(project_update_themes)
 
 
