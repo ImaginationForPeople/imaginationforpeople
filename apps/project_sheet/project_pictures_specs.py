6fcec257089d294e3c0fe9580c056b33ca9271b1
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-03-21 19:37:36 +0100

    - specific image size for ID Card - link and fixes in project listing - remove debug statement in carousel javascript

--- apps/project_sheet/project_pictures_specs.py
+++ apps/project_sheet/project_pictures_specs.py
@@ -7,12 +7,18 @@ class ResizeThumb(processors.Resize):
     height = 65 
     crop = True
 
+# first we define our thumbnail resize processor 
+class ResizeIDCard(processors.Resize): 
+    width = 137
+    height = 71 
+    crop = True
+
 # now we define a display size resize processor
 class ResizeDisplay(processors.Resize):
     width = 700
 
 # now let's create an adjustment processor to enhance the image at small sizes 
-class EnchanceThumb(processors.Adjustment): 
+class EnhanceThumb(processors.Adjustment): 
     contrast = 1.2 
     sharpness = 1.1 
 
@@ -20,10 +26,15 @@ class EnchanceThumb(processors.Adjustment):
 class Thumbnail(ImageSpec): 
     access_as = 'thumbnail_image' 
     pre_cache = True 
-    processors = [ResizeThumb, EnchanceThumb] 
+    processors = [ResizeThumb, EnhanceThumb] 
 
 # and our display spec
 class Display(ImageSpec):
     access_as = 'display' 
     increment_count = True
     processors = [ResizeDisplay]
+
+class IDCard(ImageSpec):
+    access_as = 'thumbnail_idcard'
+    pre_cache = True
+    processors = [ResizeIDCard, EnhanceThumb]
