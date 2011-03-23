e9b129fd690b6d7d0b42d5816f4c3ebcd0d01d4e
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-03-23 18:51:48 +0100

    Re-hide sidebars

--- media/js/project_sheet/sidebar.js
+++ media/js/project_sheet/sidebar.js
@@ -32,10 +32,10 @@ function hideSideBar(aSidebar){
 
 $(document).ready(function() {
     $('#opened-sidebar').css({'visibility': 'hidden','width': 260, 'height': 500});
-    //$('#opened-sidebar form').hide();
+    $('#opened-sidebar form').hide();
     
     $('#opened-sidebar-team').css({'visibility': 'hidden','width': 260, 'height': 350});
-    //$('#opened-sidebar-team form').hide();
+    $('#opened-sidebar-team form').hide();
     
     $('a.sidebar-opener.button').each(function(){
     	sidebar = $(this).attr("name");
