bb7ade4c3510a812df57037875194de9b51f068d
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-04-18 12:21:11 +0200

    Add fields to profile (fixes #69)

--- apps/member/models.py
+++ apps/member/models.py
@@ -1,13 +1,35 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import ugettext_lazy as _
 from userena.models import UserenaLanguageBaseProfile
 from django_countries import CountryField
+from apps.i4p_base.models import Location
 
 class I4pProfile(UserenaLanguageBaseProfile):
-    about = models.TextField()
-    website = models.URLField()
-    address = models.TextField()
-    country = CountryField()
-    
-    
+    #From UserenaLanguageBaseProfile.user
+    #    - first_name
+    #    - last_name
+    #    - email
+    #    - password
+    #From UserenaLanguageBaseProfile
+    #    - user language
+    #    - mugshot
+    #    - privacy (profile visibility)
+
+    GENDER_TYPE = (
+       ('M', _('male')),
+       ('F', _('female'))
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_TYPE)
+    motto = models.TextField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    website = models.URLField(verbose_name=_('website'), verify_exists=True, max_length=200, blank=True)
+    twitter = models.URLField(verbose_name=_('twitter'), verify_exists=True, max_length=200, blank=True)
+    facebook = models.URLField(verbose_name=_('facebook'), verify_exists=True, max_length=200, blank=True)
+    address = models.TextField(null=True, blank=True)
+    country = CountryField(null=True, blank=True)
+    location = models.OneToOneField(Location, verbose_name=_('location'), null=True, blank=True)
+
+
+
+
