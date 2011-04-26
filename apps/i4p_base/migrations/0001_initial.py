37b657478346daac7bdeb9369e32db4be0c985ce
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-04-26 15:15:12 +0200

    add migration for i4p_base add south field instrospection for django-licenses

---
+++ apps/i4p_base/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Location'
+        db.create_table('i4p_base_location', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('lat', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('lon', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('country', self.gf('django_countries.fields.CountryField')(max_length=2, null=True, blank=True)),
+            ('address', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('i4p_base', ['Location'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Location'
+        db.delete_table('i4p_base_location')
+
+
+    models = {
+        'i4p_base.location': {
+            'Meta': {'object_name': 'Location'},
+            'address': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'country': ('django_countries.fields.CountryField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lat': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'lon': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['i4p_base']
