097f97654b68eeb47d6c245e2149c09746c9b23a
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-05-04 17:32:51 +0200

    add Video oembed cache in admin

--- apps/project_sheet/admin.py
+++ apps/project_sheet/admin.py
@@ -5,9 +5,12 @@ from .models import I4pProject, I4pProjectTranslation
 from .models import ProjectVideo, ProjectPicture
 
 from reversion.admin import VersionAdmin
+from oembed_works.models import StoredOEmbedResponse
 
 admin.site.register(I4pProject, VersionAdmin)
 admin.site.register(I4pProjectTranslation, VersionAdmin)
 
 admin.site.register(ProjectVideo, admin.ModelAdmin)
 admin.site.register(ProjectPicture, admin.ModelAdmin)
+
+admin.site.register(StoredOEmbedResponse, admin.ModelAdmin)
