b2b71576539791572694ec62ec4dffc0d43adaa2
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-07-06 14:56:11 +0200

    use django-lint

--- run_pylint.py
+++ run_pylint.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import os
-os.system("pylint --disable=E1102 --rcfile pylint.rc apps > report.lint")
+os.system("django-lint --disable=E1102 --rcfile pylint.rc apps > report.lint")
 print "DONE."
