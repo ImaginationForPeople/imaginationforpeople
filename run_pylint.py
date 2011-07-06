7542e3d81f45485d08ecf8e54535ecee1c5f06a9
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-07-06 14:58:02 +0200

    fix django lint

--- run_pylint.py
+++ run_pylint.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import os
-os.system("django-lint --disable=E1102 --rcfile pylint.rc apps > report.lint")
+os.system("django-lint --rcfile pylint.rc apps > report.lint")
 print "DONE."
