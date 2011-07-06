f0e3570f3345456889a713ba3c3aca15164fd3ce
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-07-06 15:14:20 +0200

    pylint AND django lint

--- run_pylint.py
+++ run_pylint.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 import os
-os.system("django-lint apps > report.lint")
+os.system("pylint --disable=E1102 --rcfile pylint.rc apps > report.lint")
+os.system("django-lint apps/ >> report.lint")
 print "DONE."
