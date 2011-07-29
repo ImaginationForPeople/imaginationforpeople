ca292716416393d8c5b5afc552cb2dac15dab8c5
Alban Tiberghien <alban@nonetype.fr>
2011-07-29 18:43:24 +0200

    Re-Add imports

--- apps/project_sheet/tests.py
+++ apps/project_sheet/tests.py
@@ -2,13 +2,15 @@
 Example on how to use tests for TDD
 """
 from django.test import TestCase
-
+from django.http import QueryDict
 from apps.project_sheet.models import I4pProject, I4pProjectTranslation
 
 from .utils import create_parent_project, get_project_translation_by_slug
 from .utils import get_project_translation_from_parent, get_project_translations_from_parents
 from .utils import create_project_translation, get_or_create_project_translation_by_slug
 from .utils import get_or_create_project_translation_from_parent
+from .filters import ThemesFilterForm, FilterSet
+
 
 class TestUtils(TestCase):
     fixtures = ["test_pjsheet"]
@@ -21,8 +23,8 @@ class TestUtils(TestCase):
 
     def test_create_parent_project(self):
         project = create_parent_project()
-        self.assertTrue(project.pk  > 0)
-        
+        self.assertTrue(project.pk > 0)
+
 
     def test_get_project_translation_by_slug(self):
         # Existing project
@@ -64,18 +66,18 @@ class TestUtils(TestCase):
 
     def test_project_translation_from_parent(self):
         # Get an existing translation and project
-        translation_fr = get_project_translation_by_slug(project_translation_slug='boby-a-la-mer', 
+        translation_fr = get_project_translation_by_slug(project_translation_slug='boby-a-la-mer',
                                                          language_code='fr')
         parent = translation_fr.project
 
         # Existing one
-        requested_translation = get_project_translation_from_parent(parent=parent, 
+        requested_translation = get_project_translation_from_parent(parent=parent,
                                                                     language_code='fr')
         self.assertEqual(translation_fr, requested_translation)
 
         # If two translations have the same slug, make sure the
         # correct one is returned
-        requested_translation = get_project_translation_from_parent(parent=parent, 
+        requested_translation = get_project_translation_from_parent(parent=parent,
                                                                     language_code='zh')
         self.assertNotEqual(translation_fr, requested_translation)
 
@@ -87,7 +89,7 @@ class TestUtils(TestCase):
                           )
 
         # Non-existing one in the given language but in the fallback language
-        requested_translation = get_project_translation_from_parent(parent=parent, 
+        requested_translation = get_project_translation_from_parent(parent=parent,
                                                                     language_code='kk',
                                                                     fallback_language='fr')
         self.assertEqual(requested_translation, translation_fr)
@@ -99,7 +101,7 @@ class TestUtils(TestCase):
                           language_code='kk',
                           fallback_language='en'
                           )
-        
+
         # Parent is None
         self.assertRaises(I4pProject.DoesNotExist,
                           get_project_translation_from_parent,
@@ -109,7 +111,7 @@ class TestUtils(TestCase):
 
         # If both the language code and the fallback language don't
         # exist, make sure fallback_any works.
-        requested_translation = get_project_translation_from_parent(parent=parent, 
+        requested_translation = get_project_translation_from_parent(parent=parent,
                                                                     language_code='kk',
                                                                     fallback_language='pm',
                                                                     fallback_any=True)
@@ -117,7 +119,7 @@ class TestUtils(TestCase):
         self.assertTrue(isinstance(requested_translation, I4pProjectTranslation))
         self.assertEqual(requested_translation.project, parent)
 
-        
+
     def test_get_project_translations_from_parents(self):
         projects = I4pProject.objects.all()
 
@@ -171,14 +173,14 @@ class TestUtils(TestCase):
 
         self.assertEqual(project_translation.slug, 'new-project')
         self.assertTrue(project_translation.pk > 0)
-        
+
         # Try to create it again, the slug and model should be different
         second_project_translation = create_project_translation(language_code='fr',
                                                                 parent_project=None,
                                                                 default_title='new-project')
 
         self.assertTrue(project_translation.pk > 0)
-        self.assertNotEqual(project_translation.slug, second_project_translation.slug)        
+        self.assertNotEqual(project_translation.slug, second_project_translation.slug)
         self.assertEqual(second_project_translation.slug, 'new-project-2')
 
         self.assertNotEqual(project_translation.pk, second_project_translation.pk)
@@ -203,11 +205,11 @@ class TestUtils(TestCase):
 
         self.assertEqual(len([t for t in project.translations.all() if t.language_code == 'fr']), 1)
 
-        
+
         # Request to create a new translation
         requested_translation = create_project_translation(language_code='es',
                                                            parent_project=project)
-        
+
         self.assertTrue(requested_translation.pk > 0)
         self.assertEqual(requested_translation.language_code, 'es')
         self.assertEqual(requested_translation.project, project)
@@ -247,7 +249,7 @@ class TestUtils(TestCase):
         # already existing translation
         requested_translation = get_or_create_project_translation_from_parent(parent_project=project,
                                                                               language_code='fr')
-        
+
         self.assertEqual(len([t for t in project.translations.all() if t.language_code == 'fr']), 1)
         self.assertEqual(requested_translation, project_translation)
 
@@ -260,7 +262,7 @@ class TestUtils(TestCase):
         self.assertEqual(new_translation.slug, 'new-one')
         self.assertEqual(new_translation.project, project)
 
-        
+
 
 class TestFilters(TestCase):
     fixtures = ["test_pjsheet"]
