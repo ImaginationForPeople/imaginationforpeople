1c13627e1e5fa02a74ce0296d7c1e897dd7e3729
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-07-30 00:39:40 +0200

    fix i18n

--- apps/member/forms.py
+++ apps/member/forms.py
@@ -12,8 +12,8 @@ class I4PSignupForm(SignupForm):
     """
     Form to signup with first and last names
     """
-    first_name = forms.CharField(_(u'first name'))
-    last_name = forms.CharField(_(u'last name'))
+    first_name = forms.CharField(label=_("first name"))
+    last_name = forms.CharField(label=_("last name"))
 
     def __init__(self, *args, **kwargs):
         super(I4PSignupForm, self).__init__(*args, **kwargs)
