51f5822b46ee1c299f6ad112695138b9af23b5ca
Alban Tiberghien <alban@nonetype.fr>
2011-08-22 11:58:16 +0200

    Resolve conflict

--- apps/project_sheet/admin.py
+++ apps/project_sheet/admin.py
@@ -2,10 +2,13 @@
 Django Admin for a Project Sheet
 """
 from django.contrib import admin
+from nani import admin as nani_admin
 
 from oembed_works.models import StoredOEmbedResponse
 from reversion.admin import VersionAdmin
 
+from .models import I4pProject, I4pProjectTranslation, Objective
+from .models import ProjectVideo, ProjectPicture, ProjectMember
 from apps.partner.models import Partner
 
 from .models import I4pProject, I4pProjectTranslation
@@ -20,8 +23,13 @@ class I4pProjectAdmin(VersionAdmin):
         PartnerInline,
         )
 
+class ObjectiveAdmin(nani_admin.TranslatableAdmin):
+    list_display = ('__str__', 'all_translations')
 
 admin.site.register(I4pProject, I4pProjectAdmin)
+
+admin.site.register(Objective, ObjectiveAdmin)
+
 admin.site.register(I4pProjectTranslation, VersionAdmin)
 
 admin.site.register(ProjectVideo, admin.ModelAdmin)
