e2848a48b850aa5ad778e785bddd546bec4bb8b6
Guillaume Libersat <guillaume@fuzzyfrequency.com>
2011-09-16 17:16:24 +0200

    license software under AGPLv3 and CC BY SA. add forward variable to apache config

--- apps/i4p_base/templatetags/i4p_tags.py
+++ apps/i4p_base/templatetags/i4p_tags.py
@@ -1,3 +1,20 @@
+#-- encoding: utf-8 --
+#
+# This file is part of I4P.
+#
+# I4P is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# I4P is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero Public License for more details.
+# 
+# You should have received a copy of the GNU Affero Public License
+# along with I4P.  If not, see <http://www.gnu.org/licenses/>.
+#
 import re
 
 from django import template
--- apps/i4p_base/urls.py
+++ apps/i4p_base/urls.py
@@ -1,3 +1,4 @@
+#-- encoding: utf-8 --
 from django.conf.urls.defaults import patterns, url
 
 import views
