107879ca66d0cc4794ea1d5c33fff0b8afa8595a
Alex Marandon <contact@alexmarandon.com>
2012-01-03 17:45:22 +0100

    Enable generic OpenID authentication

--- settings.py
+++ settings.py
@@ -126,6 +126,7 @@ AUTHENTICATION_BACKENDS = (
     'social_auth.backends.facebook.FacebookBackend',
     'social_auth.backends.google.GoogleOAuth2Backend',
     'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    'social_auth.backends.OpenIDBackend',
     'userena.backends.UserenaAuthenticationBackend',
     'guardian.backends.ObjectPermissionBackend',
     'django.contrib.auth.backends.ModelBackend',
