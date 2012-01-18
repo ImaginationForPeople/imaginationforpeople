3a7e1631b615caa8b246db61df43558e00ea7f15
Alex Marandon <contact@alexmarandon.com>
2012-01-18 17:42:56 +0100

    Allow selecting multiple message recipients

--- apps/member/fields.py
+++ apps/member/fields.py
@@ -6,38 +6,45 @@
 # it under the terms of the GNU Affero Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # I4P is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero Public License
 # along with I4P.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from ajax_select import get_lookup
-from ajax_select.fields import AutoCompleteSelectField
-from django import forms
+from django.forms import ValidationError
+from django.utils.translation import ugettext as _
 
-class UserMessageRecipientField(AutoCompleteSelectField):
+from ajax_select import get_lookup
+from ajax_select.fields import AutoCompleteSelectMultipleField
+
+
+class UserMessageRecipientField(AutoCompleteSelectMultipleField):
     """
-    Subclassing ajax_select.fields.AutoCompleteSelectField because its clean
-    method returns a single object but userena.contrib.umessages needs a list of
-    user objects. In this version, our clean method returns a list. 
+    Subclassing ajax_select.fields.AutoCompleteSelectMultipleField because its
+    clean method returns a list of ids but userena.contrib.umessages requires a
+    list of user objects.
     """
     def clean(self, value):
+        """
+        Subclassed clean method which returns a list of user objects.
+        """
         if value:
             lookup = get_lookup(self.channel)
-            objs = lookup.get_objects( [ value] )
-            if len(objs) != 1:
-                # someone else might have deleted it while you were editing
-                # or your channel is faulty
-                # out of the scope of this field to do anything more than tell you it doesn't exist
-                raise forms.ValidationError(u"%s cannot find object: %s" % (lookup,value))
+            objs = lookup.get_objects(value)
+            if not objs:
+                # Someone else might have deleted them while writing the
+                # message
+                if len(value) > 1:
+                    error_message = _("Recipients not found")
+                else:
+                    error_message = _("Recipient not found")
+                raise ValidationError(error_message)
             return objs
-        else:
-            if self.required:
-                raise forms.ValidationError(self.error_messages['required'])
-            return None
-
+        elif self.required:
+            raise ValidationError(self.error_messages['required'])
+        return None
