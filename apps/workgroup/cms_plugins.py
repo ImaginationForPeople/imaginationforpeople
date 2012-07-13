83b45b08d2c3481c4e409b8ffe98054abba825df
Cyril Glapa <cyril.glapa@keuse.fr>
2012-07-13 16:08:12 +0200

    Adding a plugin to list projects that belongs to a tag

--- apps/workgroup/cms_plugins.py
+++ apps/workgroup/cms_plugins.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero Public License
 # along with I4P.  If not, see <http://www.gnu.org/licenses/>.
 #
+from django.utils import translation
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
@@ -22,8 +23,10 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 
-from .models import WorkGroupCMS, WorkGroup
+from .models import WorkGroupCMS, WorkGroup, TagCMS
 from .utils import get_ml_members
+
+from apps.project_sheet.models import I4pProject
 
 class BaseWorkGroupPlugin(CMSPluginBase):
     """
@@ -59,7 +62,7 @@ class WorkGroupPlugin(BaseWorkGroupPlugin):
     name = _("WorkGroup (un)subscribe button")
     render_template = "workgroup/cms_subscribe_button.html"
     
-   def __unicode__(self):
+    def __unicode__(self):
         return u"Workgroup Button %s" % self.workgroup.slug
     
 plugin_pool.register_plugin(WorkGroupPlugin)
@@ -72,7 +75,35 @@ class SubscribersWorkGroupPlugin(BaseWorkGroupPlugin):
     name = _("WorkGroup subscribers list")
     render_template = "workgroup/cms_workgroup_subscribers.html"
 
-   def __unicode__(self):
+    def __unicode__(self):
         return u"Workgroup Subscribers %s" % self.workgroup.slug
 
 plugin_pool.register_plugin(SubscribersWorkGroupPlugin)
+
+from apps.project_sheet.utils import get_project_translation_from_parent
+
+class ProjectsForTagPlugin(CMSPluginBase):
+    """
+    List of all projects associated to a tag
+    """
+    name = _("Projects for a tag")
+    render_template = "workgroup/projects_for_tag.html"
+    model = TagCMS
+    
+    def render(self, context, instance, placeholder):
+        language_code = translation.get_language()
+        projects = I4pProject.objects.filter(objectives=instance.tag.id)
+        i18n_projects = []
+        
+        for project in projects:
+            i18n_project = get_project_translation_from_parent(project, language_code, fallback_language="en", fallback_any="True")
+            i18n_projects.append(i18n_project)
+            
+        context["tag"] = i18n_projects
+
+        return context
+
+    def __unicode__(self):
+        return u"Tag"
+
+plugin_pool.register_plugin(ProjectsForTagPlugin)
