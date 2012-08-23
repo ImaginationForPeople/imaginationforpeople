3d4169a2ba58c84b47126cb2f8d5eb77b5683da0
Glenn Y. Rolland <glenux@glenux.net>
2012-08-23 18:40:20 +0200

    compass config.rb: make sure the new sprite name is used within css too.

--- static/config.rb
+++ static/config.rb
@@ -40,5 +40,18 @@ on_sprite_saved do |filename|
 		FileUtils.mv filename, newname
 	end
 	puts "\t  => Renamed to #{File.basename newname}"
+	newname
 end
+
+# Replace in stylesheets generated references to sprites
+# by their counterparts without the hash uniqueness.
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+    end
+  end
+end
+
 
