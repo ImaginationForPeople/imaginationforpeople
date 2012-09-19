02283f98c0e0a2c167aa39cb4d0e4e51d7b47128
Cyril Glapa <cyril.glapa@keuse.fr>
2012-09-19 18:03:22 +0200

    Updating api: replacing piston with tastypie, separating bestof and latest projects, adding a list of all projects, using slug and language_code to display detailed view and more changes

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,13 +1,13 @@
 #-- encoding: utf-8 --
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, url, include
 
-from piston.resource import Resource
+from tastypie.api import Api
 
-from .views.about import AboutHandler
-from .views.project import I4pProjectTranslationHandler
+from .views.project import I4pProjectTranslationResource
+
+v1_api = Api(api_name="v1")
+v1_api.register(I4pProjectTranslationResource())
 
 urlpatterns = patterns('',
-     url(r'^about/', Resource(AboutHandler)),
-     url(r'^project/(?P<project_id>[\d]+)$', Resource(I4pProjectTranslationHandler)),
-     url(r'^project/$', Resource(I4pProjectTranslationHandler)),
+     url(r'^', include(v1_api.urls)),
 )
--- apps/api/views/project.py
+++ apps/api/views/project.py
@@ -18,141 +18,187 @@
 
 from django.conf import settings
 
-from piston.handler import BaseHandler
-from piston.utils import throttle
+from tastypie.resources import ModelResource
 
-from apps.project_sheet.models import Answer, Objective, I4pProject, I4pProjectTranslation, Topic
+#from piston.handler import BaseHandler
+#from piston.utils import throttle
+
+from apps.project_sheet.models import Answer, Objective, I4pProject, I4pProjectTranslation, Topic,\
+    ProjectPicture, ProjectReference, ProjectVideo
 from apps.project_sheet.utils import get_project_translations_from_parents
+from django.conf.urls import url
+from tastypie import fields
+from tastypie.utils.urls import trailing_slash
+from apps.i4p_base.models import Location
+from django.contrib.auth.models import User
+from django.utils import translation
 
-class I4pProjectTranslationHandler(BaseHandler):
-    """
-    Play with projects sheets.
-    """
-    allowed_methods = ('GET',)
-    model = I4pProjectTranslation
-    project = None
-    
-    @throttle(settings.API_THROTTLE_REQUEST_COUNT, settings.API_THROTTLE_REQUEST_TIMEFRAME)
-    def read(self, request, project_id=None):
-        """Display informations about projects
+class ProjectReferenceDetailResource(ModelResource):
+    class Meta:
+        queryset = ProjectReference.objects.all()
+        include_resource_uri = False
+        fields = ['desc']
+
+class LocationListResource(ModelResource):
+    class Meta:
+        queryset = Location.objects.all()
+        include_resource_uri = False
+        fields = ['country']
+
+class LocationDetailResource(ModelResource):
+    class Meta:
+        queryset = Location.objects.all()
+        include_resource_uri = False
+        fields = ['address', 'country']
         
-        :param project_id: the id of the project to display
-        :type project_id: int
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        include_resource_uri = False
+        fields = ['username']
+    
+    def dehydrate(self, bundle):
+        bundle.data['fullname'] = bundle.obj.get_full_name()
+        return bundle
+
+class ProjectVideoDetailResource(ModelResource):
+    class Meta:
+        queryset = ProjectVideo.objects.all()
+        include_resource_uri = False
+        fields = ["video_url"]
         
-        :get lang: Used to force a language
-        :type lang: str
-        :default lang: en
+class ProjectPictureListResource(ModelResource):
+    class Meta:
+        queryset = ProjectPicture.objects.all()
+        include_resource_uri = False
+        fields = [None]
+    
+    def dehydrate(self, bundle):
+        bundle.data['thumb'] = bundle.obj.thumbnail_image.url
+        return bundle
+    
+class ProjectPictureDetailResource(ModelResource):
+    class Meta:
+        queryset = ProjectPicture.objects.all()
+        include_resource_uri = False
+        fields = ['author', 'created', 'desc', 'license', 'source']
+    
+    def dehydrate(self, bundle):
+        bundle.data['thumb'] = bundle.obj.thumbnail_image.url
+        bundle.data['url'] = bundle.obj.display.url
+        return bundle
+    
+class I4pProjectListResource(ModelResource):
+    location = fields.ForeignKey(LocationListResource, 'location', full=True, null=True)
+    pictures = fields.ToManyField(ProjectPictureListResource, 'pictures', full=True, null=True)
+    
+    class Meta:
+        queryset = I4pProject.objects.all()
+        include_resource_uri = False
+        fields = ['best_of','status']
+
+class I4pProjectDetailResource(ModelResource):
+    location = fields.ForeignKey(LocationDetailResource, 'location', full=True, null=True)
+    members = fields.ToManyField(UserResource, 'members', full=True)
+    pictures = fields.ToManyField(ProjectPictureDetailResource, 'pictures', full=True, null=True)
+    videos = fields.ToManyField(ProjectVideoDetailResource, 'videos', full=True, null=True)
+    references = fields.ManyToManyField(ProjectReferenceDetailResource, 'references', full=True, null=True)
+    
+    class Meta:
+        queryset = I4pProject.objects.all()
+        include_resource_uri = False
+        fields = ['best_of','status', 'website']
         
-        Displays best and latest projects without argument
-        Add a project id to get all data about a project
-        """
-        # TODO: Check if class attributes doesn't have problems with threads on production
-        if project_id is None:
-            language_code = request.GET.get('lang', 'en')
-            if language_code not in dict(settings.LANGUAGES) :
-                language_code = "en"
-            page = int(request.GET.get('page', 1)) - 1
-            self.__class__.fields = (
-              'id',
-              'title',
-              'baseline',
-              ('project',(
-                  ('location',(
-                      'id',
-                      'country'
-                  )),
-                  'best_of',
-                  'status',
-                  ('pictures',(
-                      'id',
-                      'thumb'
-                  ))
-              )),
-            )
-            best_projects = I4pProject.objects.filter(best_of=True)
-            localized_best_projects = get_project_translations_from_parents(best_projects, language_code, "en", True)
-            
-            latest_projects = I4pProject.objects.order_by('-created')[:10]
-            localized_latest_projects = get_project_translations_from_parents(latest_projects, language_code, "en", True)
-            
-            list_projects = {
-                "best_projects": localized_best_projects,
-                "latest_projects": localized_latest_projects
-            }
-            return list_projects
-        else:
-            self.__class__.project = I4pProjectTranslation.objects.get(pk=project_id)
-            self.__class__.fields = (
-              'id',
-              'about_section',
-              'baseline',
-              'callto_section',
-              'partners_section',
-              ('project',(
-                  'id',
-                  'best_of',
-                  'status',
-                  ('location',(
-                      'address',
-                      'country'
-                  )),
-                  ('members',(
-                      'fullname',
-                      'username'
-                  )),
-                  'objective',
-                  ('pictures',(
-                      'author',
-                      'created',
-                      'desc',
-                      'license',
-                      'source',
-                      'thumb',
-                      'url'
-                  )),
-                  'questions',
-                  ('references',(
-                      'id',
-                      'desc'
-                  )),
-                  ('videos',(
-                      'id',
-                      'video_url'
-                  )),
-                  'website'
-              )),
-              'themes',
-              'title'
-            )
-            return self.__class__.project
-    
-    @classmethod
-    def fullname(cls, anUser):
-        return anUser.get_full_name()
-    
-    @classmethod
-    def url(cls, anImageModel):
-        return anImageModel.display.url
-    
-    @classmethod
-    def questions(cls, anI4pProject):
+    def dehydrate(self, bundle):
+        objectives = bundle.obj.objectives.language(translation.get_language()).all()
+        bundle.data['objectives'] = [{"name": objective.name} for objective in objectives]
+        
         questions = []
-        for topic in Topic.objects.language(I4pProjectTranslationHandler.project.language_code).filter(site_topics=anI4pProject.topics.all()):
-            for question in topic.questions.language(I4pProjectTranslationHandler.project.language_code).all().order_by('weight'):
-                answers = Answer.objects.language(I4pProjectTranslationHandler.project.language_code).filter(project=anI4pProject.id, question=question)
+        for topic in Topic.objects.language(translation.get_language()).filter(site_topics=bundle.obj.topics.all()):
+            for question in topic.questions.language(translation.get_language()).all().order_by('weight'):
+                answers = Answer.objects.language(translation.get_language()).filter(project=bundle.obj.id, question=question)
                 questions.append({
                     "question": question.content,
                     "answer": answers and answers[0].content or None
                 })
                 
-        return questions
+        bundle.data['questions'] = questions
+        return bundle
+
+class I4pProjectTranslationResource(ModelResource):
+    project = fields.ForeignKey(I4pProjectDetailResource, use_in='detail', attribute='project', full=True)
+    about_section = fields.CharField('about_section', use_in='detail', null=True)
+    callto_section = fields.CharField('callto_section', use_in='detail', null=True)
+    partners_section = fields.CharField('partners_section', use_in='detail', null=True)
+    themes = fields.CharField('themes', use_in='detail', null=True)
     
-    @classmethod
-    def thumb(cls, anImageModel):
-        return anImageModel.thumbnail_image.url
+    class Meta:
+        queryset = I4pProjectTranslation.objects.all()
+        resource_name = 'project'
+        
+        bestof_allowed_methods = ['get']
+        latest_allowed_methods = ['get']
+        random_allowed_methods = ['get']
+        fields = ['id', 'slug','language_code','title','baseline']
+        
+    def dispatch_detail(self, request, **kwargs):
+        translation.activate(kwargs["language_code"])
+        return ModelResource.dispatch_detail(self, request, **kwargs)
     
-    # To get correct language for translated models
-    @classmethod
-    def objective(cls, anI4pProject):
-        objectives = anI4pProject.objectives.language(I4pProjectTranslationHandler.project.language_code).all()
-        return [{"name": objective.name} for objective in objectives]
+    def dispatch_bestof(self, request, **kwargs):
+        return self.dispatch('bestof', request, **kwargs)
+        
+    def dispatch_latest(self, request, **kwargs):
+        return self.dispatch('latest', request, **kwargs)
+    
+    def dispatch_random(self, request, **kwargs):
+        return self.dispatch('random', request, **kwargs)
+    
+    def prepend_urls(self):
+        array = []
+        array.append(url(r"^(?P<resource_name>%s)/(?P<language_code>[\w]+)/(?P<slug>[\w\d_.-]+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"))
+        array.append(url(r"^(?P<resource_name>%s)/bestof%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_bestof'), name="api_dispatch_bestof"))
+        array.append(url(r"^(?P<resource_name>%s)/latest%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_latest'), name="api_dispatch_latest"))
+        array.append(url(r"^(?P<resource_name>%s)/random%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_random'), name="api_dispatch_random"))
+        return array
+    
+    def define_language_code(self, request):
+        self.language_code = request.GET.get('lang', 'en')
+        if self.language_code not in dict(settings.LANGUAGES) :
+                self.language_code = "en"
+        
+        translation.activate(self.language_code)
+    
+    def get_custom(self, request, projects, **kwargs):
+        self.define_language_code(request)
+        
+        localized_latest_projects = get_project_translations_from_parents(projects, self.language_code, "en", True)
+        bundles = [self.build_bundle(obj=obj, request=request) for obj in localized_latest_projects]
+        to_be_serialized = [self.full_dehydrate(bundle, for_list=True) for bundle in bundles]
+        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
+        return self.create_response(request, to_be_serialized)
+    
+    def get_bestof(self, request, **kwargs):
+        best_projects = I4pProject.objects.filter(best_of=True)
+        return self.get_custom(request, best_projects, **kwargs);
+    
+    def get_latest(self, request, **kwargs):
+        latest_projects = I4pProject.objects.order_by('-created')[:10]
+        return self.get_custom(request, latest_projects, **kwargs)
+    
+    def get_random(self, request, **kwargs):
+        self.define_language_code(request)
+        
+        random_project = I4pProjectTranslation.objects.filter(language_code=self.language_code).order_by('?')[0]
+        bundle = self.build_bundle(obj=random_project, request=request)
+        to_be_serialized = self.full_dehydrate(bundle, for_list=False)
+        to_be_serialized = self.alter_detail_data_to_serialize(request, to_be_serialized)
+        return self.create_response(request, to_be_serialized)
+    
+    def full_dehydrate(self, bundle, for_list=False):
+        bundle = ModelResource.full_dehydrate(self, bundle, for_list=for_list)
+        bundle.related_obj = self
+        if for_list is True:
+            fk = fields.ForeignKey(I4pProjectListResource, attribute='project', full=True)
+            bundle.data['project'] = fk.dehydrate(bundle)
+        return bundle
