e823dc18e1a0d5f10214ce5851c8b87ccee0d8dc
Guillaume Libersat <guillaumelibersat@imaginationforpeople.org>
2012-10-23 20:35:48 +0200

    more template path fixes

--- apps/project_sheet/views.py
+++ apps/project_sheet/views.py
@@ -722,7 +722,7 @@ def project_sheet_history(request, project_slug):
 
 
 class ProjectRecentChangesView(TemplateView):
-    template_name = 'project_sheet/all_recent_changes.html'
+    template_name = 'project_sheet/obsolete/all_recent_changes.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProjectRecentChangesView, self).get_context_data(**kwargs)
