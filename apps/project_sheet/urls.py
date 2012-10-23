539b480cdac0ad42452a33690a2dab1cb416ff13
Guillaume Libersat <guillaumelibersat@imaginationforpeople.org>
2012-10-23 12:17:59 +0200

    make partner page works again

--- apps/project_sheet/urls.py
+++ apps/project_sheet/urls.py
@@ -15,31 +15,34 @@ PROJECT_AUTHORIZED_FIELDS = "|".join([
 ])
 
 urlpatterns = patterns('',
+    # Creating
     url(r'^add/$', views.ProjectTopicSelectView.as_view(), name='project_sheet-start'),
     url(r'^add/(?P<topic_slug>[-\w]+)/$', views.ProjectStartView.as_view(), name='project_sheet-start'),
+
+    # Listing
     url(r'^list/$', views.project_sheet_list, name='project_sheet-list'),
     url(r'^recent-changes/$', views.ProjectRecentChangesView.as_view(), name='project_sheet-recent-changes'),
 
+    # Show
+    url(r'^(?P<slug>[-\w]+)/$', views.project_sheet_show, name='project_sheet-show'),
+
+    # Edit fields
     url(r'^edit/(?P<topic_slug>[-\w]+)/(?P<field>(%s))/$' % PROJECT_AUTHORIZED_FIELDS, views.project_sheet_edit_field, name='project_sheet-edit-field'),
     url(r'^(?P<slug>[-\w]+)/edit/location/$', views.project_sheet_edit_location, name='project_sheet-instance-edit-location'),
     url(r'^(?P<slug>[-\w]+)/edit/status/$', ajax.project_sheet_edit_status, name='project_sheet-instance-edit-status'),
     url(r'^(?P<slug>[-\w]+)/edit/question/(?P<question_id>[\d]+)/$', views.project_sheet_edit_question, name='project_sheet-instance-edit-question'),
     url(r'^(?P<slug>[-\w]+)/edit/(?P<field>(%s))/$' % PROJECT_AUTHORIZED_FIELDS, views.project_sheet_edit_field, name='project_sheet-instance-edit-field'),
 
-
+    # References
     url(r'^(?P<project_slug>[-\w]+)/edit/references/$', views.project_sheet_edit_references, name='project_sheet-instance-edit-references'),
 
-    url(r'^(?P<project_slug>[-\w]+)/translate/$', views.project_sheet_create_translation, name='project_sheet-translate'),
-
+    # Tags
     url(r'^(?P<project_slug>[-\w]+)/edit/tags/$', views.project_sheet_edit_tags, name='project_sheet-instance-edit-tags'),
-                       
-    # AJAX
     url(r'^(?P<project_slug>[-\w]+)/update/related/$', ajax.project_update_related, name='project_sheet-project_update_related'),
-    
+
+    # History
     url(r'^(?P<project_slug>[-\w]+)/history/$', views.project_sheet_history, name='project_sheet-history'),
-
-    url(r'^(?P<slug>[-\w]+)/$', views.project_sheet_show, name='project_sheet-show'),
-
+                       
     # Media
     url(r'^(?P<slug>[-\w]+)/add/media/$', views.project_sheet_show, {'add_media' : True}, name='project_sheet-instance-add-media'),
     url(r'^add/media/$', views.project_sheet_add_media, name='project_sheet-add-media'),
@@ -54,8 +57,10 @@ urlpatterns = patterns('',
     url(r'^(?P<project_slug>[-\w]+)/member/delete/(?P<username>[-\w]+)/$', views.project_sheet_member_delete, name='project_sheet-instance-del-member'),
     url(r'^(?P<project_slug>[-\w]+)/member/add/$', views.project_sheet_member_add, name='project_sheet-instance-add-member'),
 
+    # Translations
+    url(r'^(?P<project_slug>[-\w]+)/translate/$', views.project_sheet_create_translation, name='project_sheet-translate'),
 
-    # Ajax views
+    # Generic Ajax views
     url(r'^start/ajax/field/save/$', ajax.project_textfield_save, name='project_sheet-ajax-field-save'),
     url(r'^start/ajax/field/load/$', ajax.project_textfield_load, name='project_sheet-ajax-field-load'),
     url(r'^(?P<project_slug>[-\w]+)/ajax/field/load/$', ajax.project_textfield_load, name='project_sheet-ajax-field-load'),
