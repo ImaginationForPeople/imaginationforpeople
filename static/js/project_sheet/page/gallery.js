4ea94792a8d6261929f26c1bf6d763a6fce6a8ee
Glenn Y. Rolland <glenux@glenux.net>
2012-11-30 16:31:40 +0100

    JS cleanup.

--- static/js/project_sheet/page/gallery.js
+++ static/js/project_sheet/page/gallery.js
@@ -8,13 +8,4 @@
 
 // FIXME: change all i4p prefixes with a local i4p-block prefix (i4p-gallery_fancy_thin...)
 
-"use strict";
-
-$(document).ready(function () {
-	$('.subheader-actions .submit-button').click(function (ev) {
-		alert('a malibu');
-		console.log('get all forms & ajax validate');
-		ev.preventDefault();
-	});
-});
 
