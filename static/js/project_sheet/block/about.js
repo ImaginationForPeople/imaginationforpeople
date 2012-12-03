3c61bad5d8855b19e38ddfb81cd517a6c5e919c4
Glenn Y. Rolland <glenux@glenux.net>
2012-12-03 22:12:21 +0100

    projectsheet: various fixes for the about edition.

--- static/js/project_sheet/block/about.js
+++ static/js/project_sheet/block/about.js
@@ -8,7 +8,11 @@
 
 // FIXME: add blue background effects on parent element of the editable content
 $(document).ready(function () {
-	$('.project-about').each(function () {
+	// var jdebug = function (str) { console.log('about: ' + str); };
+	var jdebug = function (str) { return null; };
+
+	$('.project-about').has("*[data-toggle='i4p-editable']").each(function () {
+		jdebug("each !");
 		var itemThis = this,
 			editableThis = $(itemThis).find("*[data-toggle='i4p-editable']"),
 			editableTrigger = $(editableThis).attr('data-editable-trigger');
@@ -16,16 +20,19 @@ $(document).ready(function () {
 		$(editableThis).bind(editableTrigger, function () {
 			// add edited class on elem
 			$(itemThis).addClass('editing');
+			jdebug('editing!');
 		});
 
 		$(editableThis).bind('submit', function () {
 			// remove edited class on elem
 			$(itemThis).removeClass('editing');
+			jdebug('submit!');
 		});
 
 		$(editableThis).bind('cancel', function () {
 			// remove edited class on elem
 			$(itemThis).removeClass('editing');
+			jdebug('cancel!');
 		});
 	});
 });
