468a64e05acdd4748494265f3fa5b4ce14a63f39
Freddy Limpens <freddy.limpens@gmail.com>
2013-02-21 16:31:03 +0100

    cleaned code

--- apps/forum/autocomplete_light_registry.py
+++ apps/forum/autocomplete_light_registry.py
@@ -5,7 +5,7 @@ from askbot.models.question import Thread
 class AutocompleteThread(autocomplete_light.AutocompleteModelTemplate):
     search_fields = ('title','tagnames')
     choices = Thread.objects.all()
-
+ 
 autocomplete_light.register(Thread, AutocompleteThread) 
 
         
