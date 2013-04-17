1823cf21dbd19de326d7a6efd1725d463688fd29
Freddy Limpens <freddy.limpens@gmail.com>
2013-04-17 17:23:05 +0200

    first steps in adding AJAX to search filters

---
+++ apps/i4p_base/static/js/search_filter.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){ 
+     
+   function refresh_results(){
+   // Refresh search results by submitting form via Ajax call   
+      var get_data = $("#search_form").serialize();
+      var callback_url = $("#search_form").attr("action")+get_data;
+      var updated_bar_url = callback_url // to update the bar url according to selected filters FIXME when a proper templating mechanism will be used with specific Json callback
+    
+      //$('div.content').load('/fr/project/list/?'+get_data+ ' div.content > *', function(){$("div.categories").unblock();});
+      $.get(callback_url, function(data) {
+         //replacing project listing
+         var projects_list = $(data).find('#projects_list').children();
+         $('#projects_list').empty().append(projects_list);
+         //replacing paginations
+         var pagination = $(data).find('div.projectlist_pagination').children();
+         $('div.projectlist_pagination').empty().append(pagination[0]);
+         
+         // update URL
+         var History = window.History; // Note: We are using a capital H instead of a lower h
+         if ( !History.enabled ) {// History.js is disabled for this browser. This is because we can optionally choose to support HTML4 browsers or not.
+            $("div.categories").unblock();
+            return false;
+         }
+         History.pushState('', $('title').text(), updated_bar_url); // logs {state:3}, "State 3", "?state=3"
+         
+      });
+   }
+   
+   // filter 
+   
+    $(".filtersbox_content select#id_country").change(function(){
+    	blockPanel();
+        //$("#filter_form").submit();
+        refresh_results();
+    });
+    
+    $(".filtersbox_content select#id_objectives").change(function(){
+    	blockPanel();
+        //$("#filter_form").submit();
+        refresh_results();
+    });
+    
+    $(".filters-picto input.styled").change(function(event){
+    	blockPanel();
+        //$("#filter_form").submit();
+      refresh_results();
+    });
+    
+    $(".filters-progress input").click(function(event){
+        event.stopPropagation();
+    });
+    $('.filters-progress > ul > li')
+    .css('cursor', 'pointer')
+    .click(
+      function(event){
+          var checkBoxes = $(this).find('input');
+          checkBoxes.attr("checked", !checkBoxes.attr("checked"));
+          blockPanel();
+          event.stopPropagation();
+          //$("#filter_form").submit();
+          refresh_results();
+          //checkBoxes.trigger('click');
+      }
+    );
+    
+    $(".tag_link").click(function(){
+    	blockPanel();
+        var val = $("#id_themes").val();
+        var tag_id = $(this).attr("id").replace("tag_", "");
+        
+        if($(this).hasClass("selected")){
+            $(this).removeClass("selected");
+            var new_val = val.replace(tag_id, "");
+            new_val = new_val.replace(",,", ",");
+            new_val = new_val.replace(/^,/g,'').replace(/,$/g,'')
+            $("#id_themes").val(new_val);
+        }
+        else {
+            $(this).addClass("selected");
+            var new_tag = val == "" ? tag_id : ","+tag_id;
+            $("#id_themes").val(val + new_tag);
+        }
+        //$("#filter_form").submit();
+        refresh_results();
+    });
+  
+    $(".search #id_text").example("Project Search");
+    
+    
+});
