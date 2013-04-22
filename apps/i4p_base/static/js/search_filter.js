6ce3e1572e50931dc39537093f162d5e7554d041
Freddy Limpens <freddy.limpens@gmail.com>
2013-04-22 16:53:18 +0200

    tag filtering OK

--- apps/i4p_base/static/js/search_filter.js
+++ apps/i4p_base/static/js/search_filter.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){ 
-     
+   // mirror filter tags in URL to tag list
+   rebuild_tag_list();
+   
+   function blockPanel(){
+    	$("#col").block({ message: '<p>Filtering ...</p>',
+						    		css: { 
+						                border: '1px solid #676665', 
+						                padding: '1px', 
+						                backgroundColor: '#000', 
+						                opacity: .6,
+						                color: '#fff' 
+						            }});
+    }
+   
+   
    function refresh_results(){
    // Refresh search results by submitting form via Ajax call   
+       console.log("== refrehsing results ==");
+       blockPanel();
       var get_data = $("#search_form").serialize();
       var callback_url = $("#search_form").attr("action")+"?"+get_data;
       var updated_bar_url = callback_url; // to update the bar url according to selected filters FIXME when a proper templating mechanism will be used with specific Json callback
@@ -30,34 +46,58 @@ $(document).ready(function(){
             return false;
          }
          History.pushState('', $('title').text(), updated_bar_url); // logs {state:3}, "State 3", "?state=3"
-         
+         $('#col').unblock();
       });
    }
    
-   // add tags filter to id_tags value attribute when entering text, separated by space
+   // ADDING TAGS to id_tags value attribute when entering text, separated by space
    $("#tag-search").keydown(function(e){if(e.keyCode == 13){e.preventDefault();}}); //prevent form submission
-   $("#tag-search").keyup(function(event){      
+   $("#tag-search").keyup(function(event){
+       console.log("== adding tag !! ==");      
       if(event.keyCode == 13){
         event.preventDefault();//prevent form submission
-        var tokens = $("#tag-search").val().split(" ");        
+        var tokens = $("#tag-search").val().toLowerCase().split(" ");              
         $.each(tokens, function(){
             // add each token in tag lists
             console.log(this);
-            $('#tags-list > ul').append("<li><a href='javascript:;'>"+this+"</a></li>");
+            //$('#tags-list > ul').append("<li><a href='javascript:;'>"+this+"</a></li>");
             // add each token to value attr of tags input, TODO: checking for duplicates
             var tmp = $('#id_tags').val();
-            $('#id_tags').val(tmp+" "+this);
-            // empty text field
-            $("#tag-search").val('');
+            $('#id_tags').val($.trim(tmp+" "+this));
         });
+        // empty text field and 
+        $("#tag-search").val('');  
+        rebuild_tag_list();
+        refresh_results();
       }
    });
-   // TODO remove tag from input and tag list when cliked
-   // TODO mirror tags-list with values in id_tags hidden input
-   function mirror_tag_values(){
-      // reads values
+   // REMOVING TAGS from input and tag list when cliked
+   $('#tags-list > ul > li > a').live('click', function(){
+      var tag = $(this).text();
+      console.log("removing :"+tag);
+      var old_val = $('#id_tags').val().toLowerCase();
+      var new_val = old_val.replace(tag,'');      
+      $('#id_tags').val($.trim(new_val));
+       rebuild_tag_list();
+       refresh_results();
+   });
+   // REBUILDING TAGS LIST with values in hidden input
+   function rebuild_tag_list(){
+      // reads values from id_tags and lowercase everything
+     console.log("== REBUILDING TAGS LIST ==");
+      var tokens = $('#id_tags').val().toLowerCase().split(" ");
       // replace elements in tag lists
-   
+      $('#tags-list > ul').empty();
+      $.each(tokens, function(){
+            // add each token in tag lists
+            console.log(this);
+            //TODO: skip when string is a space
+            if (this.length > 0){
+               console.log('= not space!= ');
+               $('#tags-list > ul').append("<li><a href='javascript:;'>"+this.toLowerCase()+"</a></li>");
+            }
+       });
+          
    }
    
    // Triger refresh when a checkbox is selected
