e9e5a3a6f3b5eed26d53af68c27941d5fa91759a
Mehdi Ghilaci <mehdi.ghilaci@gmail.com>
2013-04-23 16:58:49 +0200

    Newsearch WIP

--- static/js/i4p_base/search.js
+++ static/js/i4p_base/search.js
@@ -6,38 +6,20 @@ $(document).ready(function () {
 	$('#col section header').click(function(){
 		$(this).siblings('.inner').slideToggle();
 	});
-	$('.panel2').hide();
+	$('.panel1').hide();
 	$('#filter-button, #more-search .switch').click(function(){
-		$('.panel1').slideUp(200, function(){
-			$('.panel2').slideDown(600);
+		$('.panel2').slideUp(200, function(){
+			$('.panel1').slideDown(600);
 		});
 		$('#filter-button').addClass('active');
 		$('#discover-button').removeClass('active');
 	});
 	$('#discover-button').click(function(){
-		$('.panel2').slideUp(200,function() {
-			$('.panel1').slideDown(600);
+		$('.panel1').slideUp(200,function() {
+			$('.panel2').slideDown(600);
 		});
 		$('#filter-button').removeClass('active');
 		$('#discover-button').addClass('active');
-	});
-
-	// PROJECT CARD
-	$('.project-card .hover').hide();
-	//$('.project-card').on(
-	//{  mouseenter : function(){
-	//	   $(this).children('.hover').fadeIn(100);},
-	//   mouseleave: function(){
-	//	   $(this).children('.hover').fadeOut(100);}
-	//});
-	$(document).on('mouseenter', '.project-card', function() {
-      $(this).children('.hover').fadeIn(100);
-   });
-   $(document).on('mouseleave', '.project-card', function() {
-      $(this).children('.hover').fadeOut(100);
-   });
-	$('.project-card .hover').not('a').click(function(){
-		window.location.href=$(this).children('.more').children('a').attr('href');
 	});
 });
 
