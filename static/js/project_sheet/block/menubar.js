5108c545df4c8f8dd1e3bf9f989d3b04e3c69a83
Mehdi Ghilaci <mehdi.ghilaci@gmail.com>
2013-05-30 16:53:16 +0200

    Project Sheet WIP

--- static/js/project_sheet/block/menubar.js
+++ static/js/project_sheet/block/menubar.js
@@ -2,5 +2,13 @@
 
 // FIXME: add blue background effects on parent element of the editable content
 $(document).ready(function () {
-	
+	var hSize = $('#new_project_style_popup_zone').outerHeight() + $('#header-zone').outerHeight() + $('.project-subheader').outerHeight();
+	$(window).bind('scroll', function() {
+         if ($(window).scrollTop() > hSize ) {
+             $('.project-menubar').addClass('fixed');
+         }
+         else {
+             $('.project-menubar').removeClass('fixed');
+         }
+    });
 });
