08a98970d2b9f95d90b283d210ca4d5b07ef9083
Freddy Limpens <freddy.limpens@gmail.com>
2013-08-20 01:56:43 +0200

    fixed js bugs

--- static/js/project_sheet/block/subheader-readonly.js
+++ static/js/project_sheet/block/subheader-readonly.js
@@ -4,10 +4,7 @@ $(document).ready(function () {
    
    $('.infotable .map-button').click(function(){
 		$('.infotable').fadeOut(300);
-		//$('#projectLocationsMap, .infocard a.back').fadeIn(300);
-		$('#projectLocationsMap, .infocard a.back').show();
-      map.invalidateSize(false); // hack for correct leaflet size
-      //map.setView(bounds.getCenter(), map.getBoundsZoom(bounds), true)      
+		$('#projectLocationsMap, .infocard a.back').fadeIn(300);		  
 	});
 
 	$('.infocard .back').click(function(){
