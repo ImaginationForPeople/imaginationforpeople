a949c2598739a6bdb49ac994d9ce7ceaf402efe2
Cyril Glapa <cyril.glapa@keuse.fr>
2013-09-25 17:29:53 +0200

    Adding picture api file to declared api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -6,11 +6,13 @@ from tastypie.api import Api
 from .views.project import I4pProjectTranslationResource, I4pProjectEditResource
 from .views.search import SearchResource
 from .views.workgroup import WorkgroupResource
+from .views.picture import ProjectPictureResource
 
 v1_api = Api(api_name="v1")
 v1_api.register(I4pProjectTranslationResource())
 v1_api.register(SearchResource())
 v1_api.register(WorkgroupResource())
+v1_api.register(ProjectPictureResource())
 v1_api.register(I4pProjectEditResource())
 
 
