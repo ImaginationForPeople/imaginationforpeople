51fa208c9975730f92749a57fe5b8dd3e9ab883d
Benoit Grégoire <benoitg@coeus.ca>
2014-04-01 16:04:15 -0400

    Second try at removing i4p-index

--- apps/i4p_base/urls.py
+++ apps/i4p_base/urls.py
@@ -7,7 +7,6 @@ import views
 import ajax
 
 urlpatterns = patterns('',
-    url(r'^$', views.homepage, name='i4p-index'),
     url(r'^homepage/ajax/slider/bestof/$', ajax.slider_bestof, name='i4p-homepage-ajax-slider-bestof'),
     url(r'^homepage/ajax/slider/latest/$', ajax.slider_latest, name='i4p-homepage-ajax-slider-latest'),
     url(r'^homepage/ajax/slider/commented/$', ajax.slider_most_commented, name='i4p-homepage-ajax-slider-commented'),
